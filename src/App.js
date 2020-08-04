1002ddad6c477cd75c697d6a2029ba5f73e5047b
vetsinen <vetsinen@gmail.com>
2020-08-04 11:12:02 +0300

    users to local storage

--- src/App.js
+++ src/App.js
@@ -2,48 +2,46 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      users: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://yurasgr101.pythonanywhere.com/")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            users: result
-          });
-        },
-        // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
-        // чтобы не перехватывать исключения из ошибок в самих компонентах.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  render() {
-    const { error, isLoaded, users } = this.state;
-    if (error) {
-      return <div>Ошибка: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Загрузка...</div>;
-    } else {
-      return (
-        <span>{users[0].name}</span>
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            status: 'offline',
+            users: localStorage.getItem('users')
+        };
     }
-  }
+
+    componentDidMount() {
+        fetch("https://yurasgr101.pythonanywhere.com/")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        status: 'online',
+                        users: result
+                    });
+                    localStorage.setItem('users', result)
+                },
+                // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+                // чтобы не перехватывать исключения из ошибок в самих компонентах.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        status: 'offline',
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const {error, isLoaded, users, status} = this.state;
+        return (
+            <span>{users[0].name}|{status}</span>
+        );
+    }
 }
 
 export default App;
